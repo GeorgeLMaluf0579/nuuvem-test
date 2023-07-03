92c63e856804f68a6d7966d7de1a65166c699033
George L. 'Maverick' Maluf <georgelmaluf286@gmail.com>
2023-07-02 21:52:23 -0300

    Fix code coverage configuration

--- back/spec/rails_helper.rb
+++ back/spec/rails_helper.rb
@@ -5,10 +5,9 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
+
 require 'rspec/rails'
-require 'support/factory_bot'
-require 'support/database_cleaner'
-require 'support/code_coverage'
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
--- back/spec/support/code_coverage.rb
+++ back/spec/support/code_coverage.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'simplecov'
 
-Simplecov.start 'rails' do
+SimpleCov.start 'rails' do
   add_filter '/bin/'
   add_filter '/db/'
   add_filter '/spec/'
